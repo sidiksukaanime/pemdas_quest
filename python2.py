07961b0979af7a941e5cd243434cf2ceb3b83167
sidiksukaanime <110508951+sidiksukaanime@users.noreply.github.com>
2022-08-07 18:51:58 -0700

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,11 @@
+# Belajar Output
+
+nama = "sakti" # Sakti Adalah Data , Nama Adalah Variable 
+umur = 15
+alamat = "Margonda"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah" + nama)
