9a2520e923b275bd2bae9de47493675c0e83d92e
sidiksukaanime <110508951+sidiksukaanime@users.noreply.github.com>
2022-08-11 20:38:47 -0700

    Add files via upload

---
+++ UPRAK_Prima sakti_XTKJ1.py
@@ -0,0 +1,9 @@
+nama_depan = input("masukan nama depan: ")
+nama_belakang = input("masukan nama belakang: ")
+
+print("nama anda adalah", nama_depan, nama_belakang)
+
+celcius = int(input("masukin suhu celcius: "))
+ream = (4/5*celcius)
+
+print(celcius, "celcius =", ream, "reamur")
